071818b26d84ce3cead803f55e019bb676c02b8e
Matthew Eletva <s3716853@student.rmit.edu.au>
2021-06-17 01:56:23 +0000

    created quiz form

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,2 +1,38 @@
 class HomeController < ApplicationController
+  def index
+  end
+  
+  def form_submit
+    
+    user_params = quiz_params
+    categories = [{ key: :categories_linux, name: 'Linux'},
+                  { key: :categories_devops, name: 'DevOps'},
+                  { key: :categories_networking, name: 'Networking'},
+                  { key: :categories_programming, name: 'Programming'}]
+    get_request_string = ''
+    
+    categories.each do |category|
+      if user_params[category[:key]] == '1'
+        get_request_string += "categories[]=#{category[:name]}&"
+      end
+    end
+    
+    #HTML doesnt support checkbox group required, therefore need to deal with it here
+    if get_request_string.length == 0
+      flash.alert = "Please select at least one category"
+      redirect_to "/"
+    else
+      get_request_string += "difficulty=#{user_params[:difficulty]}&number=#{user_params[:number]}"
+    
+      redirect_to "/?#{get_request_string}"
+    end
+  end
+  
+  private 
+  
+    # Only allow a list of trusted parameters through.
+    def quiz_params
+      params.permit(:difficulty, :categories_linux, :categories_devops, :categories_networking, :categories_programming, :number)
+    end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root 'home#index'
-  resources :users
+  
+  post 'form_submit', to: 'home#form_submit'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
